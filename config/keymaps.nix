1bd5f2b90c9d839e93d9ae681eb892d3ac03c020
Sturmfahrt <stormfart@proton.me>
2024-05-19 23:27:23 -0400

    attempting to fix telescope keybinds

--- config/keymaps.nix
+++ config/keymaps.nix
@@ -1,9 +1,40 @@
   {
     plugins.telescope.keymaps = {
-      "<leader><leader>" = "find_files";
-      "<leader>ff" = "find_files";
-      "<leader>fg" = "live_grep";
-      "<leader>fb" = "buffers";
-      "<leader>fh" = "help_tags";
+      "<leader><leader>" = {
+        action = "find_files";
+        options = {
+          desc = "find files";
+        };
+      };
+      "<leader>ff" = {
+        action = "find_files";
+        options = {
+          desc = "find files";
+        };
+      };
+      "<leader>fg" = {
+        action = "live_grep";
+        options = {
+          desc = "live grep";
+        };
+      };
+      "<leader>fb" = {
+        action = "buffers";
+        options = {
+          desc = "find buffers";
+        };
+      };
+      "<leader>fh" = {
+        action = "help_tags";
+        options = {
+          desc = "find help";
+        };
+      };
+      "<C-p>" = {
+        action = "git_files";
+        options = {
+          desc = "Find Git Files";
+        };
+      };
     };
   }
