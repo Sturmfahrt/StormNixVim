cf298c68a11eda6969ee2d9e70a3f44c1e5d7801
Sturmfahrt <stormfart@proton.me>
2024-05-23 19:23:07 -0400

    switched to nixos 24.05

--- config/opts.nix
+++ config/opts.nix
@@ -1,8 +1,19 @@
 {
     options = {
+      number = true;
       relativenumber = true; # Show relative line numbers
-
-      shiftwidth = 2;        # Tab width should be 2
+      numberwidth = 4;
+      shiftwidth = 2; # Tab width should be 2
+      expandtab = true;
+      signcolum = "yes";
+      wrap = true;
+      updatetime = 300;
+      undofile = true;
+      smartcase = true;
+      smartindent = true;
+      splitright = true;
+      splitbelow = true;
+      fileencoding = "utf-8";
       mapleader = " ";
     };
 }
