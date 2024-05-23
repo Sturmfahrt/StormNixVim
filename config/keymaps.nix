54a9b83fbf13deef74e4fcd7472540306359a1ec
Sturmfahrt <stormfart@proton.me>
2024-05-22 22:48:45 -0400

    lets see if i can get telescope keymaps working

--- config/keymaps.nix
+++ config/keymaps.nix
@@ -1,40 +1,10 @@
-  {
-    plugins.telescope.keymaps = {
-      "<leader><leader>" = {
-        action = "find_files";
-        options = {
-          desc = "find files";
-        };
-      };
-      "<leader>ff" = {
-        action = "find_files";
-        options = {
-          desc = "find files";
-        };
-      };
-      "<leader>fg" = {
-        action = "live_grep";
-        options = {
-          desc = "live grep";
-        };
-      };
-      "<leader>fb" = {
-        action = "buffers";
-        options = {
-          desc = "find buffers";
-        };
-      };
-      "<leader>fh" = {
-        action = "help_tags";
-        options = {
-          desc = "find help";
-        };
-      };
-      "<C-p>" = {
-        action = "git_files";
-        options = {
-          desc = "Find Git Files";
-        };
-      };
-    };
-  }
+{
+  globals.mapleader = " ";
+  keymaps = [
+    {
+      mode = "n";
+      key = "<leader><leader>";
+      action = "+find/file";
+    }
+  ];
+}
